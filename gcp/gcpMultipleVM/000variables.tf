9444f85643e7e8a61995292bdaa32dd107040df1
danny <danny@danny>
2019-07-12 09:29:00 +0100

    added setup for 2 vms

---
+++ gcp/gcpMultipleVM/000variables.tf
@@ -0,0 +1,77 @@
+variable "project" {
+	default = "danny-1562750321"
+}
+
+variable "name" {
+	default = "default"
+}
+
+variable "name2" {
+	default = "default2"
+}
+
+variable "machine_type" {
+	default = "n1-standard-1"
+}
+
+variable "zone" {
+	default = "europe-west2-c"
+}
+
+variable "image" {
+	default = "ubuntu-1804-bionic-v20190628"
+
+}
+
+variable "network" {
+	default = "default"
+}
+
+variable "ssh_user" {
+	default = "terraform"
+}
+
+variable "public_key" {
+	default = "~/.ssh/id_rsa.pub"
+}
+
+variable "private_key" {
+	default = "~/.ssh/id_rsa"
+}
+
+variable "package_manager" {
+	default = "apt"
+}
+
+variable "update_packages" {
+	default = {
+		"apt" = "sudo apt update && sudo apt upgrade -y"
+		"yum" = "sudo yum update -y"
+	}	
+}
+
+variable "packages" {
+	default = [
+		"wget"
+	]
+}
+
+variable "install_packages" {
+	default = {
+		"yum" = "sudo yum install -y",
+		"apt" = "sudo apt install -y"
+	}
+}
+
+variable "scripts_express" {
+	default = []
+}
+
+variable "scripts_angular" {
+	default = []
+}
+
+variable "allowed_ports" {
+	default = ["22", "8080", "4200"]
+}
+
---
+++ gcp/gcpMultipleVM/variables/apiServer.tfvars
@@ -0,0 +1,8 @@
+name = "danny-1562750321"
+packages = [
+	"git"
+]
+scripts_express = [
+	"scripts/apiVMSetup.sh"
+]
+	
